cdbdd71218b87afa7cbe32cd3cf53080d7ceb697
alexperez46358-a11y <alexperez46.358@gmail.com>
2026-01-31 18:10:39 +0000

    dawg completed

---
+++ my-dog.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(200);
+  circle(50, 50, 35);
+  rect(50, 60, 50, 40);
+  rect(50, 100, 10, 40);
+  rect(60, 100, 10, 40);
+  rect(80, 100, 10, 40);
+  rect(90, 100, 10, 40);
+}
